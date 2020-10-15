911e2ce79ee837b6ccb8a12060e6dc1948d6f164
addamsv <swares@mail.ru>
2020-10-15 19:12:19 +0300

    fix: 0.0 appearance of a previouse arrg

--- calculator/rsscalculator.js
+++ calculator/rsscalculator.js
@@ -7,7 +7,7 @@
  * @license GPL 3.0 http://www.gnu.org/licenses/gpl.html
  * @Version: 1.0.0
  *
- * Require: modern browsers
+ * Require: modern browsers (NOT IE BECAUSE OF 'extends' does't work over there)
  * Build: 80 (27-SEP-2020)
  * 
  */
@@ -73,10 +73,10 @@ class CalculatorView {
         ob.appendChild(newNode);
 
         this.makeButton(ob, 'AC', 'AC', 'span-two fn-btn ac-dell-btn');
-        this.makeButton(ob, 'DEL', 'DEL', 'span-two fn-btn ac-dell-btn');
-        this.makeButton(ob, '^', 'pow','fn-btn');
+        this.makeButton(ob, 'x', 'DEL', 'span-two fn-btn ac-dell-btn del-btn');
+        this.makeButton(ob, '^', 'pow','fn-btn');//X<sup><small>n</small></sup> may be but... you know what...
         this.makeButton(ob, '√', 'root','fn-btn');
-        this.makeButton(ob, '+/-','','fn-btn');
+        this.makeButton(ob, '±','+/-','fn-btn');
         this.makeButton(ob, '/','','fn-btn');
         this.makeButton(ob, '7');
         this.makeButton(ob, '8');
@@ -92,7 +92,7 @@ class CalculatorView {
         this.makeButton(ob, '-','','fn-btn');
         this.makeButton(ob, '.');
         this.makeButton(ob, '0');
-        this.makeButton(ob,  "", '=', 'span-two equal-btn');//&#61;
+        this.makeButton(ob,  "", '=', 'span-two equal-btn');
      }
 
     /**
@@ -221,8 +221,7 @@ class CalculatorModel extends CalculatorView {
                 this.currentOperandTextElement.innerText = '√';
             }
             return;
-        }
-        
+        }  
     }
 
    /**
@@ -242,6 +241,7 @@ class CalculatorModel extends CalculatorView {
             this.currentOperand = 0;
             this.isEqualsBtnWasClicked = false;
         }
+        /* max allowed dig */
         if(this.currentOperand.toString().length > 14){
             return;
         }
@@ -284,6 +284,9 @@ class CalculatorModel extends CalculatorView {
         }
         this.currentOperand = 0;
         if(!this.isAPIorTestMode){
+            if(this.previousOperand == 0){
+                this.previousOperand = 0;
+            }
             this.previousOperandTextElement.innerText = (this.operation && this.previousOperand !== '') ? this.makeSeparatedDigit(this.previousOperand) + ' ' + this.operation : this.makeSeparatedDigit(this.previousOperand);
             this.currentOperandTextElement.innerText = '0';
         }
@@ -315,24 +318,17 @@ class CalculatorModel extends CalculatorView {
     }
       
     /**
+    * Sorry someone for that kind of code 
 	* @protected Compute
 	* @return {String}
 	*/
     compute(){
         let computation;
         let current = (parseFloat(this.currentOperand) ? parseFloat(this.currentOperand) : 0);
-
         const PREV = (parseFloat(this.previousOperand) ? parseFloat(this.previousOperand) : 0);
 
-
-        // if(!this.isSqrtOperation && (isNaN(PREV) || isNaN(current))){
-        //     return;
-        // }
         if(this.isSqrtOperation){
             if(this.currentOperand < 0){
-                // alert ('Incorrect argument!');
-                // current = current * (-1);
-                // this.currentOperand = current;
                 this.clear();
                 if(!this.isAPIorTestMode){
                     this.currentOperandTextElement.innerText = 'Error: Incorrect argument!';
@@ -348,9 +344,7 @@ class CalculatorModel extends CalculatorView {
             this.isSqrtOperation = false;
             current = Math.sqrt(this.sqrtOperand, 2); //current = Math.sqrt(current, this.sqrtOperand);
         }
-        // if(!this.isPowOperation && (isNaN(PREV) || isNaN(current))){
-        //     return;
-        // }
+
         if(this.isPowOperation){
             if(this.powOperand===''){
                 this.powOperand = 0;
@@ -405,9 +399,7 @@ class CalculatorModel extends CalculatorView {
         }
         if(computation.toString().slice('').includes('.')){
             if(computation >= 1000000000){
-                // 999999999999.3
                 computation = (Math.round(computation*100))/100;
-
             }
             else if(computation < 1000000000 && computation > 1000000){
                 computation = (Math.round(computation*1000))/1000;
@@ -426,9 +418,11 @@ class CalculatorModel extends CalculatorView {
             this.currentOperand = 0;
         }
         if(!this.isAPIorTestMode){
+            if(this.previousOperand == 0){
+                this.previousOperand = 0;
+            }
             this.previousOperandTextElement.innerText = (this.operation ? this.previousOperand + ' ' +  this.operation + ' '  : '') + (this.sqrtOperand!=='' ? '√' + this.sqrtOperand :  this.currentOperand ) + (this.powOperand!=='' ? '^' + this.powOperand : '') + ' = ' + parseFloat(this.makeValidDigitString(computation));
         }
-        // this.previousOperandTextElement.innerText = (this.operation ? PREV + ' ' +  this.operation + ' '  : '') + (this.sqrtOperand!=='' ? '√' + this.sqrtOperand :  current ) + (this.powOperand!=='' ? '^' + this.powOperand : '') + ' = ' + parseFloat(this.makeValidDigitString(computation));
         this.currentOperand = parseFloat(this.makeValidDigitString(computation));
         if(!this.isAPIorTestMode){
             this.currentOperandTextElement.innerText = this.currentOperand;
@@ -477,6 +471,9 @@ class CalculatorModel extends CalculatorView {
 	*/
     displayPrev(){
         if(!this.isAPIorTestMode){
+            if(this.previousOperand == 0){
+                this.previousOperand = 0;
+            }
             this.previousOperandTextElement.innerText = (this.operation && this.previousOperand != '') ? this.makeSeparatedDigit(this.previousOperand) + ' ' + this.operation : this.makeSeparatedDigit(this.previousOperand);
         }
     }
@@ -652,7 +649,18 @@ class Calculator extends CalculatorModel {
 }
 
 
-
+/**
+ *      Calculator
+ * 
+ * 
+ *        
+ *         API
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
 class CalculatorAPI extends CalculatorModel {
 
     constructor(apiTestMode) {
@@ -660,10 +668,10 @@ class CalculatorAPI extends CalculatorModel {
     }
 
     /**
-	* @public	Controlls all button we have
+	* @public	Use for tests, further dev with Sequence (complicated stuff) and another JS app
 	*
-	* @param  {string} btn - Object of the Button
-	* @return {void}
+	* @param  {String} sequence - String Sequence of operators and operands separated by one-gap between, with equal (=) operator at the end
+	* @return {String} result
 	*/
     setSequence(sequence = '1 + 1 ='){
         let result = '';
@@ -711,128 +719,3 @@ class CalculatorAPI extends CalculatorModel {
 
     }
 }
-
-
-/* 
-Базовая функциональность
-1 + 2 => 3
-23 + 69.5 => 92.5
-74 * 3 - 5 => 217
-2 + 3 => 5 продолжаем ввод 4 => 4 - после равно следующая цифра перезаписывает результат
-есть кнопка, позволяющая очистить результат
-Дополнительные математические операции
-25 √ => 5 или √ 25 => 5 - любой вариант правильный
-9 √ + 1 => 4 или √ 9 + 1 => 4 - любой вариант правильный
-2 ^ 2 => 4
-15 ^ 3 => 3375
-10.1 ^ 3 => 1030.301
-Действия с отрицательными числами
--9 / -3 => 3
-2 + -2 => 0
-2 / -2 => -1
--9 ^ 3 => -729
--9 √ => уведомление об ошибке или √ - 9 => уведомление об ошибке - любой вариант правильный
-Действия с дробями
-0.1 + 0.2 => 0.3
-0.4 - 0.1 => 0.3
-0.0004 + 0.0004 => 0.0008
--0.1 * 0.2 => -0.02
--0.15 + -0.15 => -0.3 - а не - 0.30
-
-
-Extended Tests:
-8 + - / * 2 = 16
-0 / 2 = 0
-1 + = = = = 1
-3 + 4 ^ 2 = 19 ^ 2 = 361 Multiple degree ERROR Solved
-
-2 + 5 del √ + 5 => Nan Solved
-√ => 0 Solved
-9 * / del / сбрасывает пред операнд Solved
-0 ^ 0 => 1^0=1 False Solved
-^ =>  1^0=1 False Solved
-del ^ => Solved
-5 del ^ => 1^0=1 False Solved
-2 ^ 1 +/- ISSUE changed prev Solved
-2 ^ 2 +/- Solved
-√ 9 + 1 => Solved
-2 ^ 2 + 1 =>  Solved
- 
-ISSUE:
-2 ^ +/-  it adds 0
- */
-
-
-const TEST = new CalculatorAPI(true);
-
-let test = '';
-let br = "\n";
-
-test+='/* Базовая функциональность */' + br;
-test+=(TEST.setSequence('1 + 2 =') === 3) + ' 1 + 2 = 3'+br;
-test+=(TEST.setSequence('23 + 69.5 =') === 92.5 ) + ' 23 + 69.5 =92.5' + br;
-test+=(TEST.setSequence('74 * 3 - 5 =') === 217) + ' 74 * 3 - 5 = 217' + br;
-test+=(TEST.setSequence('2 + 3 = 4 =') === 4) + ' 2 + 3 = 4 = 4 ' + 'после равно следующая цифра перезаписывает результат' + br;
-test+=(TEST.setSequence('5 AC') === 0) + ' AC - есть кнопка позволяющая очистить результат' + br;
-
-test+=br + '/* Дополнительные математические операции */' + br;
-test+=(TEST.setSequence('25 root') === 5) + ' 25 root = 5' + br;
-test+=(TEST.setSequence('AC root 25 =') === 5) +  ' AC root 25 = 5' + br;
-test+=(TEST.setSequence('9 root + 1 =') === 4) +  ' 9 root + 1 = 4' + br;
-test+=(TEST.setSequence('2 pow 2 =') === 4) +  ' 2 pow 2 = 4' + br;
-test+=(TEST.setSequence('15 pow 3 =') === 3375) +  ' 15 pow 3 = 3375' + br;
-test+=(TEST.setSequence('10.1 pow 3 =') === 1030.301) +  ' 10.1 pow 3 = 1030.301' + br;
-
-test+=br + '/* Действия с отрицательными числами */' + br;
-test+=(TEST.setSequence('9 +/- / 3 +/- =') === 3) +  ' 9 +/- / 3 +/- = 3' + br;
-test+=(TEST.setSequence('2 + 2 +/- =') === 0) +  ' 2 + 2 +/- = 0' + br;
-test+=(TEST.setSequence('2 / 2 +/- =') === -1) +  ' 2 / 2 +/- = -1' + br;
-test+=(TEST.setSequence('9 +/- pow 3 =') === -729) +  ' 9 +/- pow 3 = -729' + br;
-test+=(TEST.setSequence('9 +/- root') === 'Error: Incorrect argument!') + ' 9 +/- root -> Error: Incorrect argument!' + br;
-test+=(TEST.setSequence('AC root 9 +/- =') === 'Error: Incorrect argument!') + ' root 9 +/- = ->Error: Incorrect argument!' + br;
-test+=(TEST.setSequence('AC root 9 +/- + 1 =') === 'Error: Incorrect argument!') + ' root 9 +/-  + 1 = ->Error: Incorrect argument!' + br;
-
-test+=br + '/* Действия с дробями: */' + br;
-test+=(TEST.setSequence('0.1 + 0.2 =') === 0.3 )+  ' 0.1 + 0.2 = 0.3' + br;
-test+=(TEST.setSequence('0.4 - 0.1 =') === 0.3) +  ' 0.4 - 0.1 = 0.3' + br;
-test+=(TEST.setSequence('0.3 - 0.2 =') === 0.1) +  ' 0.3 - 0.2 = 0.1' + br;
-test+=(TEST.setSequence('0.0004 + 0.0004 =') === 0.0008 ) +  ' 0.0004 + 0.0004 = 0.0008' + br;
-test+=(TEST.setSequence('0.1 +/- * 0.2 =') === -0.02) +  ' 0.1 +/- * 0.2 = -0.02' + br;
-test+=(TEST.setSequence('0.15 +/- + 0.15 +/- =') === -0.3) +  ' 0.15 +/- + 0.15 +/- = -0.3' + br;
-
-test+=br + '/* Big Digit Tests: */' + br;
-test+=(TEST.setSequence('9999999999999999999999999999999999999999999999999999999999 + 1 =') === 'Error: Incorrect argument!') + ' 9999999999999999999999999999999999999999999999999999999999 + 1 = -> Error: Incorrect argument! You arent alowed to text such a big digit within RSSCalculatorAPI' + br;
-test+=(TEST.setSequence('99999999999999 * 99999999 =') === 99999998999990) + '  99999999999999 * 99999999 = 99999998999990' + br;
-test+=(TEST.setSequence('999999999999.3 - 0.2 =') === 999999999999.1) + '  99999999999999.3 - 0.2 = 99999999999999.1' + br;
-test+=(TEST.setSequence('10000000000000 + 1 =') === 1000000000001) + ' 10000000000000 + 1 = 10000000000001' + br;
-test+=(TEST.setSequence('10 pow 10000 =') === 'Infinity') + ' 10 pow 10000 = Infinity' + br;
-test+=(TEST.setSequence('10 pow 100 =') === '1.0000000000000002e+100') + ' 10 pow 100 = 1.0000000000000002e+100' + br;
-
-test+=br + '/* Crush Tests: */' + br;
-test+=(TEST.setSequence('9 / 0 =') === 'Error: Incorrect argument!') + ' 9 / 0 = Error: Incorrect argument!' + br;
-test+=(TEST.setSequence('0 / 2 =') === 0) + ' 0 / 2 = 0'+ br;
-test+=(TEST.setSequence('8 + - / * 2 =') === 16) + ' 8 + - / * 2 = 16' + br;
-test+=(TEST.setSequence('1 + = = = =') === 1) + ' 1 + = = = = 1 ' + br;
-test+=(TEST.setSequence('3 + 4 pow 2 = pow 2 =') === 361) + ' 3 + 4 pow 2 = pow 2 = 361' + br;
-test+=(TEST.setSequence('3 + 4 pow 2 = 19 pow 2 =') === 361 ) + ' 3 + 4 pow 2 = 19 pow 2 = 361 (Multiple degree ERROR Solved)' + br;
-test+=(TEST.setSequence('2 + 5 DEL root + 4 =') === 6) + ' ' + '2 + 5 del root + 4 = 6' + br;
-test+=(TEST.setSequence('AC root =') === 0 )+ ' ' + 'root = 0' + br;
-test+=(TEST.setSequence('2 pow 2 + 1 =') === 5) + ' ' + '2 ^ 2 + 1 = 5' + br;
-test+=(TEST.setSequence('2 pow 2 + 1 =') === 5) + ' ' + '2 ^ 2 + 1 = 5' + br;
-test+=(TEST.setSequence('9 * / del / + 1 =') === 10) + ' ' + '9 * / del / + 1 = 10' + 'сбрасывает пред операн' + br 
-test+=(TEST.setSequence('AC 9 root + 1 =') === 4) + ' ' + '√ 9 + 1 = 4' + br;
-test+=(TEST.setSequence('AC 2 pow 1 +/-  =') === 0.5) + ' ' + '2 ^ 1 +/- = 0.5' + br;
-test+=(TEST.setSequence('5 DEL pow =') === 1) + ' ' + '5 del ^ = 1' + br;
-test+=(TEST.setSequence('AC pow =') === 1) + ' ' + '^ = 1' + br;
-test+=(TEST.setSequence('0 pow 0 =') === 1) + ' ' + '0^0 = 1' + br;
-test+=(TEST.setSequence('100 pow 2 root =') === 100) + ' ' + ' 100^2√ = 100' + br;
-// let computation = 100000000000000.300000000002 - 100000000000000.200000000002;
-let computation = 0.33 + 0.22;
-// if(computation>100000000000000){
-//     computation =( Math.round(computation*1000))/1000;
-// }
-// else{
-    computation = (Math.round(computation*100000000000000))/100000000000000;
-// }
-test+=(computation);
-console.log(test);
