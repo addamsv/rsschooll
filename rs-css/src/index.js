ce2c02bbdddd3a8d51ac4979ce02e8a4a8e21d03
addamsv <swares@mail.ru>
2020-12-05 16:34:31 +0300

    refactor: separated to different modules; Build one css file

---
+++ rs-css/src/index.js
@@ -0,0 +1,10 @@
+import './css/common.style.css';
+import './css/header.style.css';
+import './css/main.style.css';
+import './css/footer.style.css';
+
+import './images/github.png';
+
+import App from './modules/controller';
+
+new App().init();
