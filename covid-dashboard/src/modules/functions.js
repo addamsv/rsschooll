7375185a96fac054c693609cff0706d357374bf1
addamsv <swares@mail.ru>
2020-12-15 18:41:27 +0300

    refactor: removed all unnecessary functions

--- covid-dashboard/src/modules/functions.js
+++ covid-dashboard/src/modules/functions.js
@@ -1,25 +1,5 @@
-const BASIC_DELAY = 250;
-const MENU = document.getElementsByClassName('menu')[0];
-
 export default class Functions {
   init() {
     return true;
-  }
-
-  toggleMenu() {
-    if (this.closeMenu()) {
-      return;
-    }
-    setTimeout(() => MENU.classList.add('menu-open'), BASIC_DELAY);
-    MENU.classList.remove('menu-close');
-  }
-
-  closeMenu() {
-    if (MENU.classList.contains('menu-open')) {
-      MENU.classList.remove('menu-open');
-      setTimeout(() => MENU.classList.add('menu-close'), BASIC_DELAY);
-      return true;
-    }
-    return false;
   }
 }
