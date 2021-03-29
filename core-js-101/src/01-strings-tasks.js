09e2edccf2662b16901b9c67159573f5a3882d9f
addamsv <swares@mail.ru>
2021-03-29 13:31:15 +0300

    init: coreJS

---
+++ core-js-101/src/01-strings-tasks.js
@@ -0,0 +1,314 @@
+/* *******************************************************************************************
+ *                                                                                           *
+ * Plese read the following tutorial before implementing tasks:                              *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String   *
+ *                                                                                           *
+ ******************************************************************************************* */
+
+
+/**
+ * Returns the result of concatenation of two strings.
+ *
+ * @param {string} value1
+ * @param {string} value2
+ * @return {string}
+ *
+ * @example
+ *   'aa', 'bb' => 'aabb'
+ *   'aa',''    => 'aa'
+ *   '',  'bb'  => 'bb'
+ */
+function concatenateStrings(value1, value2) {
+  return `${value1}${value2}`; // throw new Error('Not implemented');
+}
+
+
+/**
+ * Returns the length of given string.
+ *
+ * @param {string} value
+ * @return {number}
+ *
+ * @example
+ *   'aaaaa' => 5
+ *   'b'     => 1
+ *   ''      => 0
+ */
+function getStringLength(value) {
+  return value.length; // throw new Error('Not implemented');
+}
+
+/**
+ * Returns the result of string template and given parameters firstName and lastName.
+ * Please do not use concatenation, use template string :
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/template_strings
+ *
+ * @param {string} firstName
+ * @param {string} lastName
+ * @return {string}
+ *
+ * @example
+ *   'John','Doe'      => 'Hello, John Doe!'
+ *   'Chuck','Norris'  => 'Hello, Chuck Norris!'
+ */
+function getStringFromTemplate(firstName, lastName) {
+  return `Hello, ${firstName} ${lastName}!`; // throw new Error('Not implemented');
+}
+
+/**
+ * Extracts a name from template string 'Hello, First_Name Last_Name!'.
+ *
+ * @param {string} value
+ * @return {string}
+ *
+ * @example
+ *   'Hello, John Doe!' => 'John Doe'
+ *   'Hello, Chuck Norris!' => 'Chuck Norris'
+ */
+function extractNameFromTemplate(value) {
+  return value.slice(7, value.length - 1); // throw new Error('Not implemented');
+}
+
+
+/**
+ * Returns a first char of the given string.
+ *
+ * @param {string} value
+ * @return {string}
+ *
+ * @example
+ *   'John Doe'  => 'J'
+ *   'cat'       => 'c'
+ */
+function getFirstChar(value) {
+  return value.slice(0, 1); // throw new Error('Not implemented');
+}
+
+/**
+ * Removes a leading and trailing whitespace characters from string.
+ *
+ * @param {string} value
+ * @return {string}
+ *
+ * @example
+ *   '  Abracadabra'    => 'Abracadabra'
+ *   'cat'              => 'cat'
+ *   '\tHello, World! ' => 'Hello, World!'
+ */
+function removeLeadingAndTrailingWhitespaces(value) {
+  return value.trim(); // throw new Error('Not implemented');
+}
+
+/**
+ * Returns a string that repeated the specified number of times.
+ *
+ * @param {string} value
+ * @param {string} count
+ * @return {string}
+ *
+ * @example
+ *   'A', 5  => 'AAAAA'
+ *   'cat', 3 => 'catcatcat'
+ */
+function repeatString(value, count) {
+  return Array(count).fill(value).join(''); // throw new Error('Not implemented');
+}
+
+/**
+ * Remove the first occurrence of string inside another string
+ *
+ * @param {string} str
+ * @param {string} value
+ * @return {string}
+ *
+ * @example
+ *   'To be or not to be', 'not'  => 'To be or  to be'
+ *   'I like legends', 'end' => 'I like legs',
+ *   'ABABAB','BA' => 'ABAB'
+ */
+function removeFirstOccurrences(str, value) {
+  return str.replace(value, ''); // return str.split(value).join(''); // throw new Error('Not implemented');
+}
+
+/**
+ * Remove the first and last angle brackets from tag string
+ *
+ * @param {string} str
+ * @return {string}
+ *
+ * @example
+ *   '<div>' => 'div'
+ *   '<span>' => 'span'
+ *   '<a>' => 'a'
+ */
+function unbracketTag(str) {
+  // return str.split('<').join('').split('>').join('');
+  // return str.replace(/[><]/g, '');
+  return str.slice(1, str.length - 1); // throw new Error('Not implemented');
+}
+
+
+/**
+ * Converts all characters of the specified string into the upper case
+ *
+ * @param {string} str
+ * @return {string}
+ *
+ * @example
+ *   'Thunderstruck' => 'THUNDERSTRUCK'
+ *  'abcdefghijklmnopqrstuvwxyz' => 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+ */
+function convertToUpperCase(str) {
+  return str.toUpperCase(); // throw new Error('Not implemented');
+}
+
+/**
+ * Extracts e-mails from single string with e-mails list delimeted by semicolons
+ *
+ * @param {string} str
+ * @return {array}
+ *
+ * @example
+ *   'angus.young@gmail.com;brian.johnson@hotmail.com;bon.scott@yahoo.com'
+ *   => [
+ *      'angus.young@gmail.com',
+ *      'brian.johnson@hotmail.com',
+ *      'bon.scott@yahoo.com'
+ *   ],
+ *   'info@gmail.com' => ['info@gmail.com']
+ */
+function extractEmails(str) {
+  return str.split(';'); // throw new Error('Not implemented');
+}
+
+/**
+ * Returns the string representation of rectangle with specified width and height
+ * using pseudograhic chars
+ *
+ * @param {number} width
+ * @param {number} height
+ * @return {string}
+ *
+ * @example
+ *
+ *            '┌────┐\n'+
+ *  (6,4) =>  '│    │\n'+
+ *            '│    │\n'+
+ *            '└────┘\n'
+ *
+ *  (2,2) =>  '┌┐\n'+
+ *            '└┘\n'
+ *
+ *             '┌──────────┐\n'+
+ *  (12,3) =>  '│          │\n'+
+ *             '└──────────┘\n'
+ *
+ */
+function getRectangleString(width, height) {
+  let out = '';
+  const line = Array(width - 2).fill('─').join('');
+  out += `┌${line}┐\n`;
+  if (height > 2) {
+    const filledArea = Array(width - 2).fill(' ').join('');
+    for (let i = 2; i < height; i += 1) {
+      out += `│${filledArea}│\n`;
+    }
+  }
+  out += `└${line}┘\n`;
+  return out; // throw new Error('Not implemented');
+}
+
+
+/**
+ * Encode specified string with ROT13 cipher
+ * See details:  https://en.wikipedia.org/wiki/ROT13
+ *
+ * @param {string} str
+ * @return {string}
+ *
+ * @example
+ *
+ *   'hello' => 'uryyb'
+ *   'Why did the chicken cross the road?' => 'Jul qvq gur puvpxra pebff gur ebnq?'
+ *   'Gb trg gb gur bgure fvqr!' => 'To get to the other side!'
+ *   'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+ *    => 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'
+ *
+ */
+const alph = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
+const rot13 = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'.split('');
+function encodeToRot13(str) {
+  return str.split('').map((val) => (alph.indexOf(val) !== -1 ? rot13[alph.indexOf(val)] : val)).join('');
+  // throw new Error('Not implemented');
+}
+
+/**
+ * Returns true if the value is string; otherwise false.
+ * @param {string} value
+ * @return {boolean}
+ *
+ * @example
+ *   isString() => false
+ *   isString(null) => false
+ *   isString([]) => false
+ *   isString({}) => false
+ *   isString('test') => true
+ *   isString(new String('test')) => true
+ */
+function isString(value) {
+  return typeof value === 'string' || value instanceof String; // throw new Error('Not implemented');
+}
+
+
+/**
+ * Returns playid card id.
+ *
+ * Playing cards inittial deck inclides the cards in the following order:
+ *
+ *  'A♣','2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','Q♣','K♣',
+ *  'A♦','2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','Q♦','K♦',
+ *  'A♥','2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥',
+ *  'A♠','2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠'
+ *
+ * (see https://en.wikipedia.org/wiki/Standard_52-card_deck)
+ * Function returns the zero-based index of specified card in the initial deck above.
+ *
+ * @param {string} value
+ * @return {number}
+ *
+ * @example
+ *   'A♣' => 0
+ *   '2♣' => 1
+ *   '3♣' => 2
+ *     ...
+ *   'Q♠' => 50
+ *   'K♠' => 51
+ */
+const deck = ['A♣', '2♣', '3♣', '4♣', '5♣', '6♣', '7♣', '8♣', '9♣', '10♣', 'J♣', 'Q♣', 'K♣',
+  'A♦', '2♦', '3♦', '4♦', '5♦', '6♦', '7♦', '8♦', '9♦', '10♦', 'J♦', 'Q♦', 'K♦',
+  'A♥', '2♥', '3♥', '4♥', '5♥', '6♥', '7♥', '8♥', '9♥', '10♥', 'J♥', 'Q♥', 'K♥',
+  'A♠', '2♠', '3♠', '4♠', '5♠', '6♠', '7♠', '8♠', '9♠', '10♠', 'J♠', 'Q♠', 'K♠'];
+function getCardId(value) {
+  // return deck.reduce((acc, currentVal, index) => acc + (currentVal === value ? index : ''), 0);
+  // return deck.lastIndexOf(value);
+  return deck.indexOf(value);
+}
+
+
+module.exports = {
+  concatenateStrings,
+  getStringLength,
+  getStringFromTemplate,
+  extractNameFromTemplate,
+  getFirstChar,
+  removeLeadingAndTrailingWhitespaces,
+  repeatString,
+  removeFirstOccurrences,
+  unbracketTag,
+  convertToUpperCase,
+  extractEmails,
+  getRectangleString,
+  encodeToRot13,
+  isString,
+  getCardId,
+};
---
+++ core-js-101/src/08-conditions-n-loops-tasks.js
@@ -0,0 +1,530 @@
+/* *************************************************************************************************
+ *                                                                                                *
+ * Plese read the following tutorial before implementing tasks:                                   *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Control_flow_and_error_handling  *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration              *
+ *                                                                                                *
+ ************************************************************************************************ */
+
+
+/**
+ * Returns the 'Fizz','Buzz' or an original number using the following rules:
+ * 1) return original number
+ * 2) but if number multiples of three return 'Fizz'
+ * 3) for the multiples of five return 'Buzz'
+ * 4) for numbers which are multiples of both three and five return 'FizzBuzz'
+ *
+ * @param {number} num
+ * @return {any}
+ *
+ * @example
+ *   2 =>  2
+ *   3 => 'Fizz'
+ *   5 => 'Buzz'
+ *   4 => 4
+ *  15 => 'FizzBuzz'
+ *  20 => 'Buzz'
+ *  21 => 'Fizz'
+ *
+ */
+function getFizzBuzz(num) {
+  if ((num % 3 === 0 && num % 5 === 0)) {
+    return 'FizzBuzz';
+  }
+  if (num % 3 === 0) {
+    return 'Fizz';
+  }
+  if (num % 5 === 0) {
+    return 'Buzz';
+  }
+  return num;
+}
+
+
+/**
+ * Returns the factorial of the specified integer n.
+ *
+ * @param {number} n
+ * @return {number}
+ *
+ * @example:
+ *   1  => 1
+ *   5  => 120
+ *   10 => 3628800
+ */
+function getFactorial(n) {
+  return (n !== 1) ? n * getFactorial(n - 1) : 1;
+}
+
+
+/**
+ * Returns the sum of integer numbers between n1 and n2 (inclusive).
+ *
+ * @param {number} n1
+ * @param {number} n2
+ * @return {number}
+ *
+ * @example:
+ *   1,2   =>  3  ( = 1+2 )
+ *   5,10  =>  45 ( = 5+6+7+8+9+10 )
+ *   -1,1  =>  0  ( = -1 + 0 + 1 )
+ */
+function getSumBetweenNumbers(n1, n2) {
+  const shift = 1;
+  const difference = n2 - n1;
+  const nInterval = Array(difference + shift).fill(0).map((item, index) => index + n1);
+  return nInterval.reduce((accumulator, item) => accumulator + item);
+}
+
+
+/**
+ * Returns true, if a triangle can be built with the specified sides a, b, c
+ * and false in any other ways.
+ *
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @return {bool}
+ *
+ * @example:
+ *   1,2,3    =>  false
+ *   3,4,5    =>  true
+ *   10,1,1   =>  false
+ *   10,10,10 =>  true
+ */
+function isTriangle(a, b, c) {
+  return ((a + b) > c && (a + c) > b && (b + c) > a);
+}
+
+
+/**
+ * Returns true, if two specified axis-aligned rectangles overlap, otherwise false.
+ * Each rectangle representing by object
+ *  {
+ *     top: 5,
+ *     left: 5,
+ *     width: 20,
+ *     height: 10
+ *  }
+ *
+ *  (5;5)
+ *     -------------
+ *     |           |
+ *     |           |  height = 10
+ *     -------------
+ *        width=20
+ *
+ * NOTE: Please use canvas coordinate space (https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Drawing_shapes#The_grid),
+ * it differs from Cartesian coordinate system.
+ *
+ * @param {object} rect1
+ * @param {object} rect2
+ * @return {bool}
+ *
+ * @example:
+ *   { top: 0, left: 0, width: 10, height: 10 },
+ *   { top: 5, left: 5, width: 20, height: 20 }    =>  true
+ *
+ *   { top: 0, left: 0, width: 10, height: 10 },
+ *   { top:20, left:20, width: 20, height: 20 }    =>  false
+ *
+ */
+function doRectanglesOverlap(rect1, rect2) {
+  const x1 = rect1.left;
+  const x2 = rect1.left + rect1.width;
+  const y1 = rect1.top;
+  const y2 = rect1.top + rect1.height;
+  const x3 = rect2.left;
+  const x4 = rect2.left + rect2.width;
+  const y3 = rect2.top;
+  const y4 = rect2.top + rect2.height;
+  return (x1 < x4) && (x3 < x2) && (y1 < y4) && (y3 < y2);
+}
+
+/**
+ * Returns true, if point lies inside the circle, otherwise false.
+ * Circle is an object of
+ *  {
+ *     center: {
+ *       x: 5,
+ *       y: 5
+ *     },
+ *     radius: 20
+ *  }
+ *
+ * Point is object of
+ *  {
+ *     x: 5,
+ *     y: 5
+ *  }
+ *
+ * @param {object} circle
+ * @param {object} point
+ * @return {bool}
+ *
+ * @example:
+ *   { center: { x:0, y:0 }, radius:10 },  { x:0, y:0 }     => true
+ *   { center: { x:0, y:0 }, radius:10 },  { x:10, y:10 }   => false
+ *
+ */
+function isInsideCircle(circle, point) {
+  const line = Math.abs(circle.center.x - point.x) ** 2 + Math.abs(circle.center.y - point.y) ** 2;
+  return Math.sqrt(line) < circle.radius;
+}
+
+
+/**
+ * Returns the first non repeated char in the specified strings otherwise returns null.
+ *
+ * @param {string} str
+ * @return {string}
+ *
+ * @example:
+ *   'The quick brown fox jumps over the lazy dog' => 'T'
+ *   'abracadabra'  => 'c'
+ *   'entente' => null
+ */
+function findFirstSingleChar(str) {
+  const charsArray = str.split('');
+  function isCharExistYet(i) {
+    const charsNextIndex = charsArray.indexOf(charsArray[i]) + 1;
+    return (charsArray.indexOf(charsArray[i], charsNextIndex) !== -1);
+  }
+  for (let i = 0; i < charsArray.length; i += 1) {
+    if (!isCharExistYet(i)) {
+      return charsArray[i];
+    }
+  }
+  return null;
+}
+
+
+/**
+ * Returns the string representation of math interval,
+ * specified by two points and include / exclude flags.
+ * See the details: https://en.wikipedia.org/wiki/Interval_(mathematics)
+ *
+ * Please take attention, that the smaller number should be the first in the notation
+ *
+ * @param {number} a
+ * @param {number} b
+ * @param {bool} isStartIncluded
+ * @param {bool} isEndIncluded
+ * @return {string}
+ *
+ * @example
+ *   0, 1, true, true   => '[0, 1]'
+ *   0, 1, true, false  => '[0, 1)'
+ *   0, 1, false, true  => '(0, 1]'
+ *   0, 1, false, false => '(0, 1)'
+ * Smaller number has to be first :
+ *   5, 3, true, true   => '[3, 5]'
+ *
+ */
+function getIntervalString(a, b, isStartIncluded, isEndIncluded) {
+  const center = `${a}${b}`.split('').sort().join(', ');
+  return (isStartIncluded ? '[' : '(') + center + (isEndIncluded ? ']' : ')');
+}
+
+
+/**
+ * Reverse the specified string (put all chars in reverse order)
+ *
+ * @param {string} str
+ * @return {string}
+ *
+ * @example:
+ * 'The quick brown fox jumps over the lazy dog' => 'god yzal eht revo spmuj xof nworb kciuq ehT'
+ * 'abracadabra' => 'arbadacarba'
+ * 'rotator' => 'rotator'
+ * 'noon' => 'noon'
+ */
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+
+
+/**
+ * Reverse the specified integer number (put all digits in reverse order)
+ *
+ * @param {number} num
+ * @return {number}
+ *
+ * @example:
+ *   12345 => 54321
+ *   1111  => 1111
+ *   87354 => 45378
+ *   34143 => 34143
+ */
+function reverseInteger(num) {
+  return num.toString().split('').reverse().join('');
+}
+
+
+/**
+ * Validates the CCN (credit card number) and return true if CCN is valid
+ * and false otherwise.
+ *
+ * See algorithm here : https://en.wikipedia.org/wiki/Luhn_algorithm
+ *
+ * @param {number} cnn
+ * @return {boolean}
+ *
+ * @example:
+ *   79927398713      => true
+ *   4012888888881881 => true
+ *   5123456789012346 => true
+ *   378282246310005  => true
+ *   371449635398431  => true
+ *
+ *   4571234567890111 => false
+ *   5436468789016589 => false
+ *   4916123456789012 => false
+ */
+function isCreditCardNumber(ccn) {
+  const arr = ccn.toString().split('').reverse();
+  arr.some((element, index) => {
+    if (index % 2 !== 0) {
+      arr[index] = (2 * element > 9) ? 2 * element - 9 : 2 * element;
+    } else {
+      arr[index] = element;
+    }
+    return false;
+  });
+  return arr.reduce((accum, element) => parseInt(accum, 10) + parseInt(element, 10)) % 10 === 0;
+}
+
+/**
+ * Returns the digital root of integer:
+ *   step1 : find sum of all digits
+ *   step2 : if sum > 9 then goto step1 otherwise return the sum
+ *
+ * @param {number} n
+ * @return {number}
+ *
+ * @example:
+ *   12345 ( 1+2+3+4+5 = 15, 1+5 = 6) => 6
+ *   23456 ( 2+3+4+5+6 = 20, 2+0 = 2) => 2
+ *   10000 ( 1+0+0+0+0 = 1 ) => 1
+ *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
+ */
+function getDigitalRoot(num) {
+  return (num.toString().split('').length !== 1) ? getDigitalRoot(num.toString().split('').reduce((accum, element) => accum + Number(element), 0)) : num;
+}
+
+/**
+ * Returns true if the specified string has the balanced brackets and false otherwise.
+ * Balanced means that is, whether it consists entirely of pairs of opening/closing brackets
+ * (in that order), none of which mis-nest.
+ * Brackets include [],(),{},<>
+ *
+ * @param {string} str
+ * @return {boolean}
+ *
+ * @example:
+ *   '' => true
+ *   '[]'  => true
+ *   '{}'  => true
+ *   '()   => true
+ *   '[[]' => false
+ *   ']['  => false
+ *   '[[][][[]]]' => true
+ *   '[[][]][' => false
+ *   '{)' = false
+ *   '{[(<{[]}>)]}' = true
+ */
+function isBracketsBalanced(str) {
+  if (str.length % 2 !== 0) {
+    return false;
+  }
+  const openBracketsArray = '{[(<{['.split('');
+  const closedBracketsArray = ']}>)]}'.split('').reverse();
+  const opened = [];
+  function isBracketOpened(bracket) {
+    return openBracketsArray.indexOf(bracket) !== -1;
+  }
+  function getOpenedBracketThroughClosed(closedBracket) {
+    const indexOfOpenedBracket = closedBracketsArray.indexOf(closedBracket);
+    if (indexOfOpenedBracket === -1) {
+      return false;
+    }
+    return openBracketsArray[indexOfOpenedBracket];
+  }
+  const isNotPassed = str.split('').some((element) => {
+    if (isBracketOpened(element)) {
+      opened.push(element);
+      return false;
+    }
+    if (opened[opened.length - 1] === getOpenedBracketThroughClosed(element)) {
+      opened.pop();
+      return false;
+    }
+    /* exception!!! should return true */
+    return true;
+  });
+  if (isNotPassed) {
+    return false;
+  }
+  return opened.length === 0;
+}
+
+
+/**
+ * Returns the string with n-ary (binary, ternary, etc, where n <= 10)
+ * representation of specified number.
+ * See more about
+ * https://en.wikipedia.org/wiki/Binary_number
+ * https://en.wikipedia.org/wiki/Ternary_numeral_system
+ * https://en.wikipedia.org/wiki/Radix
+ *
+ * @param {number} num
+ * @param {number} n, radix of the result
+ * @return {string}
+ *
+ * @example:
+ *   1024, 2  => '10000000000'
+ *   6561, 3  => '100000000'
+ *    365, 2  => '101101101'
+ *    365, 3  => '111112'
+ *    365, 4  => '11231'
+ *    365, 10 => '365'
+ */
+function toNaryString(num, n) {
+  const divisionRemainder = [];
+  function addDigitToRemainder(digit) {
+    divisionRemainder.push(digit % n);
+    return Math.floor(digit / n);
+  }
+  let digit = num;
+  while (digit >= n) {
+    digit = addDigitToRemainder(digit);
+  }
+  addDigitToRemainder(digit);
+  return `${divisionRemainder.reverse().join('')}`;
+}
+
+
+/**
+ * Returns the commom directory path for specified array of full filenames.
+ *
+ * @param {array} pathes
+ * @return {string}
+ *
+ * @example:
+ *   ['/web/images/image1.png', '/web/images/image2.png']  => '/web/images/'
+ *   ['/web/assets/style.css', '/web/scripts/app.js',  'home/setting.conf'] => ''
+ *   ['/web/assets/style.css', '/.bin/mocha',  '/read.me'] => '/'
+ *   ['/web/favicon.ico', '/web-scripts/dump', '/webalizer/logs'] => '/'
+ */
+function getCommonDirectoryPath(pathes) {
+  const pattern = pathes.sort()[0];
+
+  function isLetterExistEverywhere(letter, index) {
+    return pathes.every((element) => element[index] === letter);
+  }
+  function isLastElementFits(resultPath) {
+    const directElementArray = resultPath.split('/');
+    const patternElArray = pattern.split('/');
+    return patternElArray.every((element, index) => directElementArray[index] === element);
+  }
+  function getResultWithoutLastEl(fullPath) {
+    const finishResult = fullPath.split('/');
+    finishResult[finishResult.length - 1] = '';
+    return finishResult.join('/');
+  }
+  function getFullResult() {
+    let outPath = '';
+    for (let letterNumber = 0; letterNumber < pattern.length; letterNumber += 1) {
+      if (!isLetterExistEverywhere(pattern[letterNumber], letterNumber)) {
+        break;
+      }
+      outPath += pattern[letterNumber];
+    }
+    return outPath;
+  }
+
+  const result = getFullResult();
+  if (!isLastElementFits(result)) {
+    return getResultWithoutLastEl(result);
+  }
+  return result;
+}
+
+
+/**
+ * Returns the product of two specified matrixes.
+ * See details: https://en.wikipedia.org/wiki/Matrix_multiplication
+ *
+ * @param {array} m1
+ * @param {array} m2
+ * @return {array}
+ *
+ * @example:
+ *   [[ 1, 0, 0 ],       [[ 1, 2, 3 ],           [[ 1, 2, 3 ],
+ *    [ 0, 1, 0 ],   X    [ 4, 5, 6 ],     =>     [ 4, 5, 6 ],
+ *    [ 0, 0, 1 ]]        [ 7, 8, 9 ]]            [ 7, 8, 9 ]]
+ *
+ *                        [[ 4 ],
+ *   [[ 1, 2, 3]]    X     [ 5 ],          =>     [[ 32 ]]
+ *                         [ 6 ]]
+ *
+ */
+function getMatrixProduct(/* m1, m2 */) {
+  throw new Error('Not implemented');
+}
+
+
+/**
+ * Returns the evaluation of the specified tic-tac-toe position.
+ * See the details: https://en.wikipedia.org/wiki/Tic-tac-toe
+ *
+ * Position is provides as 3x3 array with the following values: 'X','0', undefined
+ * Function should return who is winner in the current position according to the game rules.
+ * The result can be: 'X','0',undefined
+ *
+ * @param {array} position
+ * @return {string}
+ *
+ * @example
+ *
+ *   [[ 'X',   ,'0' ],
+ *    [    ,'X','0' ],       =>  'X'
+ *    [    ,   ,'X' ]]
+ *
+ *   [[ '0','0','0' ],
+ *    [    ,'X',    ],       =>  '0'
+ *    [ 'X',   ,'X' ]]
+ *
+ *   [[ '0','X','0' ],
+ *    [    ,'X',    ],       =>  undefined
+ *    [ 'X','0','X' ]]
+ *
+ *   [[    ,   ,    ],
+ *    [    ,   ,    ],       =>  undefined
+ *    [    ,   ,    ]]
+ *
+ */
+function evaluateTicTacToePosition(/* position */) {
+  throw new Error('Not implemented');
+}
+
+
+module.exports = {
+  getFizzBuzz,
+  getFactorial,
+  getSumBetweenNumbers,
+  isTriangle,
+  doRectanglesOverlap,
+  isInsideCircle,
+  findFirstSingleChar,
+  getIntervalString,
+  reverseString,
+  reverseInteger,
+  isCreditCardNumber,
+  getDigitalRoot,
+  isBracketsBalanced,
+  toNaryString,
+  getCommonDirectoryPath,
+  getMatrixProduct,
+  evaluateTicTacToePosition,
+};
